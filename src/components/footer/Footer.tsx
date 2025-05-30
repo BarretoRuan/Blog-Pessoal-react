79c46b4454e6393045f0a803625c48888478c7fd
Ruan Barreto <barretoruan65@gmail.com>
2025-05-30 10:31:46 -0300

    Deploy do Projeto

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,30 +1,44 @@
-import { FacebookLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from '@phosphor-icons/react'
+import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { type ReactNode, useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 
 function Footer() {
 
     let data = new Date().getFullYear()
 
-    return (
-        <>
+    const { usuario } = useContext(AuthContext)
+
+    let component: ReactNode
+
+    if (usuario.token !== "") {
+
+        component = (
+
             <div className="flex justify-center bg-indigo-900 text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
-                            Blog Pessoal Generation | Copyright: {data}
-                        </p>
+                        Blog Pessoal Generation | Copyright: {data}
+                    </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
-                     <div className='flex gap-2'>
-	                     <a href="https://www.linkedin.com/in/ruan-barreto/" target="_blank">
-    	                     <LinkedinLogoIcon size={48} weight='bold' />
-                          </a>
-                          <a href="https://www.instagram.com/_ruan_bs/" target="_blank">
-    	                      <InstagramLogoIcon size={48} weight='bold' />
-                          </a>
-                          <a href="https://www.facebook.com/ruan.barreto.379322?locale=pt_BR" target="_blank">
-    	                      <FacebookLogoIcon size={48} weight='bold' />
-	                      </a>
-                     </div>
+                    <div className='flex gap-2'>
+                        <a href="https://www.linkedin.com/in/ruan-barreto/" target="_blank">
+                            <LinkedinLogo size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.instagram.com/_ruan_bs/" target="_blank">
+                            <InstagramLogo size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.facebook.com/ruan.barreto.379322" target="_blank">
+                            <FacebookLogo size={48} weight='bold' />
+                        </a>
+                    </div>
                 </div>
             </div>
+
+        )
+    }
+    return (
+        <>
+            { component }
         </>
     )
 }
